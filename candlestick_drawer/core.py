8d97e5257686a696d3864010f6645187c78d1edc
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-01-05 16:43:28 +0330

    done: data entry

--- candlestick_drawer/core.py
+++ candlestick_drawer/core.py
@@ -7,13 +7,14 @@ import mplfinance as mpf
 class CandleStickCore():
     def __init__(self, symbol, timeframe, resolution):
         self.symbol = symbol
-        self.timeframe = timeframe * 60 * 60 #hours to seconds, since we need 24 hours and above, the base is on 24 hours for now.
+        self.timeframe = timeframe
+        self.to_timeframe = timeframe * 60 * 60 #hours to seconds, since we need 24 hours and above, the base is on 24 hours for now.
         self.resolution = resolution
     
-    def get_data_from_nobitex():
+    def get_data_from_nobitex(self):
         params = {
-        "symbol": symbol,
-        "resolution": resolution,
-        "from": from_timestamp,
-        "to": to_timestamp
+        "symbol": self.symbol,
+        "resolution": self.esolution,
+        "from": self.timeframe,
+        "to": self.to_timeframe
     }
