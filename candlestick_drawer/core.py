dbcdeffa41b994d21aa223ee404188ba001c6482
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-01-05 16:34:47 +0330

    working on it

--- candlestick_drawer/core.py
+++ candlestick_drawer/core.py
@@ -1,8 +1,12 @@
 # Core File
 import requests
+import time
 import pandas as pd 
 import mplfinance as mpf
 
 class CandleStickCore():
-    def __init__(self):
+    def __init__(self, symbol, timeframe):
+        self.symbol = symbol
+    
+    def get_data_from_nobitex():
         pass
