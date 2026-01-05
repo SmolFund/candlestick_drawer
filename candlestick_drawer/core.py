b11dcddc8293c9e280f9ae116e008d1bab6bb35e
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-01-05 16:35:41 +0330

    working on it

--- candlestick_drawer/core.py
+++ candlestick_drawer/core.py
@@ -7,6 +7,7 @@ import mplfinance as mpf
 class CandleStickCore():
     def __init__(self, symbol, timeframe):
         self.symbol = symbol
+        self.timeframe = timeframe * 60 * 60 #hours to seconds, since we need 24 hours and above, the base is on 24 hours for now.
     
     def get_data_from_nobitex():
         pass
